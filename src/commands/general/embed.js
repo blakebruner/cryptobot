add44b5ca1d86fd3f4fb39127d92a55bd2ed51c9
Blake Bruner <blakebruner00@gmail.com>
2024-12-03 22:19:14 -0800

    ping cmd

---
+++ src/commands/general/embed.js
@@ -0,0 +1,40 @@
+// at the top of your file
+const { EmbedBuilder } = require('discord.js');
+
+/** @type {import("commandkit").CommandData}  */
+export const data = {
+	name: "embed",
+	description: "Test an embed"
+}
+
+/**
+ * @param {import("commandkit").SlashCommandProps} param0 
+ */
+export const run = ({ interaction, client }) => {
+	// inside a command, event listener, etc.
+	const exampleEmbed = new EmbedBuilder()
+		.setColor(0x0099FF)
+		.setTitle('Some title')
+		.setURL('https://discord.js.org/')
+		.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
+		.setDescription('Some description here')
+		.setThumbnail('https://i.imgur.com/AfFp7pu.png')
+		.addFields(
+			{ name: 'Regular field title', value: 'Some value here' },
+			{ name: '\u200B', value: '\u200B' },
+			{ name: 'Inline field title', value: 'Some value here', inline: true },
+			{ name: 'Inline field title', value: 'Some value here', inline: true },
+		)
+		.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
+		.setImage('https://i.imgur.com/AfFp7pu.png')
+		.setTimestamp()
+		.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
+
+	interaction.reply({ embeds: [exampleEmbed] })
+	console.log(interaction)
+}
+
+/** @type {import("commandkit").CommandOptions} */
+export const options = {
+	// https://commandkit.js.org/typedef/CommandOptions
+}
--- src/commands/general/ping.js
+++ src/commands/general/ping.js
@@ -1,4 +1,3 @@
-const { EmbedBuilder } = require('discord.js');
 /** @type {import("commandkit").CommandData}  */
 export const data = {
 	name: "ping",
@@ -9,31 +8,10 @@ export const data = {
  * @param {import("commandkit").SlashCommandProps} param0 
  */
 export const run = ({ interaction, client }) => {
-
-	// inside a command, event listener, etc.
-	const exampleEmbed = new EmbedBuilder()
-		.setColor(0x0099FF)
-		.setTitle('Some title')
-		.setURL('https://discord.js.org/')
-		.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-		.setDescription('Some description here')
-		.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-		.addFields(
-			{ name: 'Regular field title', value: 'Some value here' },
-			{ name: '\u200B', value: '\u200B' },
-			{ name: 'Inline field title', value: 'Some value here', inline: true },
-			{ name: 'Inline field title', value: 'Some value here', inline: true },
-		)
-		.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-		.setImage('https://i.imgur.com/AfFp7pu.png')
-		.setTimestamp()
-		.setFooter({ text: 'Some footer text here', iconURL: 'https://i.imgur.com/AfFp7pu.png' });
-
-	interaction.reply({ embeds: [exampleEmbed] });
-	// interaction.reply({
-	// 	content: `🏓 WebSocket: \`${client.ws.ping}ms\``,
-	// 	ephemeral: true
-	// })
+	interaction.reply({
+		content: `🏓 WebSocket: \`${client.ws.ping}ms\``,
+		ephemeral: true
+	})
 }
 
 /** @type {import("commandkit").CommandOptions} */

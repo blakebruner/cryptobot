21c6b759cb92133e7f06b11eb413cbe1898083d4
2Apples2Apples <sirthomasandy@gmail.com>
2024-12-12 18:59:58 -0800

    models and wallet

---
+++ src/models/Wallet.js
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose"
+
+const transaction = {
+
+}
+
+export const Wallet = model("Wallet", new Schema({
+	currency: {
+		type: String
+	},
+  address: {
+    type: String
+  },
+  lastUpdate: {
+    type: Date,
+    defualt: Date.now
+  }, 
+  transactions: {
+    type: [transaction]
+  }
+}))
